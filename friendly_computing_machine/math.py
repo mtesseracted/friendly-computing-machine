bc2ca8c3d7a381e993f9dbcdb097b172f75993e3
Daniel Smith <malorian@me.com>
2017-07-25 14:28:46 -0400

    Starts the FCM

---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,15 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+def add(arg1, arg2):
+    """
+    Function that adds two arguements
+    """
+    return 2 + 5
+
+def mult(arg1, arg2):
+    """
+    Function that multiplies two  arguements
+    """
+    return 2 * 5
